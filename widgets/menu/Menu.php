8be51fccf6aea07d4024736003dc17b8866c4a6d
Pavel <pfilsx@gmail.com>
2017-12-28 12:24:29 +0300

    test new menu

--- widgets/menu/Menu.php
+++ widgets/menu/Menu.php
@@ -19,6 +19,9 @@ class Menu extends Widget
 
     public $itemOptions = [];
 
+    protected $_class = 'crl-menu';
+    protected $_class_horizontal = 'crl-menu-horizontal';
+
     protected $_currentUrl;
     protected $_currentRoute;
 
@@ -35,41 +38,58 @@ class Menu extends Widget
         ob_start();
         ob_implicit_flush(false);
         if ($this->orientation == 'horizontal'){
-            $this->renderHorizontal();
-        } else {
-            $this->renderVertical();
+            $this->_class .= " {$this->_class_horizontal}";
         }
+        $this->render();
         return ob_get_clean();
     }
 
-    protected function renderHorizontal(){
-        echo Html::startTag('div', ArrayHelper::merge_recursive($this->options, ['class' => ' crm-menu']));
+    protected function render(){
+        echo Html::startTag('div', ArrayHelper::merge_recursive($this->options, ['class' => $this->_class]));
         foreach ($this->items as $item){
-            if (!isset($item['label']) || !isset($item['url'])){
+            if (!isset($item['label']) || (!isset($item['url']) && (!isset($item['items']) || !is_array($item['items'])))){
                 throw new ErrorException('Invalid parameters passed to Menu::widget items');
             }
-            echo Html::tag('a', $item['label'], ArrayHelper::merge_recursive($this->itemOptions, [
-                'class' => ($this->_currentUrl == $item['url'] || $this->_currentRoute == $item['url']
-                    ? 'active'
-                    : ''),
-                'href' => $item['url']
-            ]));
+            if ($this->orientation == 'horizontal'){
+                $this->renderHorizontalItem($item);
+            } else {
+                $this->renderVerticalItem($item);
+            }
         }
         echo Html::endTag('div');
     }
-    protected function renderVertical(){
-        echo Html::startTag('div', ArrayHelper::merge_recursive($this->options, ['class' => ' crm-menu crl-menu-horizontal']));
-        foreach ($this->items as $item){
-            if (!isset($item['label']) || !isset($item['url'])){
-                throw new ErrorException('Invalid parameters passed to Menu::widget items');
+
+    protected function renderHorizontalItem($item){
+        if (isset($item['items'])){
+            echo Html::startTag('div', ArrayHelper::merge_recursive(['class' => 'crl-menu-item'], $this->itemOptions));
+            echo Html::startTag('div', ['class' => 'crl-menu-subitems']);
+            foreach ($item['items'] as $subItem){
+                echo Html::tag('a', $subItem['label'], ArrayHelper::merge_recursive([
+                    'href' => $subItem['url'],
+                    'class' => 'crl-menu-subitem'
+                ], $subItem['options']));
             }
-            echo Html::tag('a', $item['label'], ArrayHelper::merge_recursive($this->itemOptions, [
-                'class' => ($this->_currentUrl == $item['url'] || $this->_currentRoute == $item['url']
-                    ? 'active'
-                    : ''),
+            echo Html::endTag('div');
+            echo Html::endTag('div');
+        } else {
+            echo Html::tag('a', ArrayHelper::merge_recursive([
+                'class' => 'crl-menu-item',
                 'href' => $item['url']
-            ]));
+            ], $this->itemOptions));
         }
-        echo Html::endTag('div');
+//        echo Html::tag('a', $item['label'], ArrayHelper::merge_recursive($this->itemOptions, [
+//            'class' => ($this->_currentUrl == $item['url'] || $this->_currentRoute == $item['url']
+//                ? 'active'
+//                : ''),
+//            'href' => $item['url']
+//        ]));
+    }
+    protected function renderVerticalItem($item){
+        echo Html::tag('a', $item['label'], ArrayHelper::merge_recursive($this->itemOptions, [
+            'class' => ($this->_currentUrl == $item['url'] || $this->_currentRoute == $item['url']
+                ? 'active'
+                : ''),
+            'href' => $item['url']
+        ]));
     }
 }
