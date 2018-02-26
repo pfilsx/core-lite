fdcf94c8eda101f55aa86d8a57504e5fd154f0f7
Pavel <pfilsx@gmail.com>
2018-02-26 12:47:02 +0300

    test new assets system

--- components/AssetBundle.php
+++ components/AssetBundle.php
@@ -19,6 +19,9 @@ abstract class AssetBundle extends BaseObject
 
     public $sourcePath;
 
+    protected $_jsAssets = [];
+    protected $_cssAssets = [];
+
     /**
      * @return array
      */
@@ -51,7 +54,23 @@ abstract class AssetBundle extends BaseObject
 
     public final static function register()
     {
-        App::$instance->assetManager->registerBundle(get_called_class());
+        //App::$instance->assetManager->registerBundle(get_called_class());
+        $className = get_called_class();
+        if (in_array($className, App::$instance->assetManager->registeredBundles)){
+            return;
+        }
+        /**
+         * @var AssetBundle $bundle
+         */
+        $bundle = new $className();
+        foreach ($bundle->depends() as $depend){
+            if (class_exists($depend) && is_subclass_of($depend, AssetBundle::className())){
+                $depend::register();
+            }
+        }
+        $bundle->publish(App::$instance->assetManager);
+        $bundle->registerBundle(App::$instance->view);
+        App::$instance->assetManager->registeredBundles[] = $className;
     }
 
     /**
@@ -62,17 +81,37 @@ abstract class AssetBundle extends BaseObject
             $publishResult = $am->publish($this->sourcePath);
             $this->basePath = $publishResult[0];
             $this->baseUrl = $publishResult[1];
+            foreach ($this->jsAssets() as $jsPath){
+                $this->_jsAssets[$this->basePath.'/'.ltrim($jsPath, '/')] = $this->baseUrl.'/'.ltrim($jsPath, '/');
+            }
+            foreach ($this->cssAssets() as $cssPath){
+                $this->_cssAssets[$this->basePath.'/'.ltrim($cssPath, '/')] = $this->baseUrl.'/'.ltrim($cssPath, '/');
+            }
         } else {
             if ($this->basePath !== null){
                 $this->basePath = FileHelper::normalizePath(Core::getAlias($this->basePath));
             }
             foreach ($this->jsAssets() as $i => $jsPath){
                 $publishResult = $am->publish($this->basePath.'/'.ltrim($jsPath, '/'));
-                
+                $this->_jsAssets[$publishResult[0]] = $publishResult[1];
             }
             foreach ($this->cssAssets() as $cssPath){
-
+                $publishResult = $am->publish($this->basePath.'/'.ltrim($cssPath, '/'));
+                $this->_cssAssets[$publishResult[0]] = $publishResult[1];
             }
+        }
+    }
+
+    /**
+     * @param View $view
+     */
+    protected final function registerBundle($view){
+        if ($view == null){ return; }
+        foreach ($this->_jsAssets as $asset){
+            $view->registerJsFile($asset);
+        }
+        foreach ($this->_cssAssets as $asset){
+            $view->registerCssFile($asset);
         }
     }
 }
